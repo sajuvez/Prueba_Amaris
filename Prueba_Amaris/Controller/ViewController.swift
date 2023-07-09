a24792c061b8ac1978164a081a3dd158faadbd3e
WilsonTique <sajuvez.1996@gmail.com>
2023-07-09 14:09:39 -0500

    Initial Commit

---
+++ Prueba_Amaris/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba_Amaris
+//
+//  Created by Wilson Jair Tique Aguja on 9/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("holi")
+    }
+
+
+}
+
